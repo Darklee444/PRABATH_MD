96453d916f821c00209debd90392a431d23d5b7b
Darklee444 <142809525+Darklee444@users.noreply.github.com>
2023-09-02 14:10:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,30 +7,30 @@ global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
 global.port= process.env.PORT || 5000
 global.email = 'sam@sec.live'
-global.github = 'https://github.com/prabathLK/PRABATH-MD'
+global.github = 'https://github.com/A-S-W-I-N-S-P-A-R-K-Y/X-BOT-MD'
 global.location = 'Sultanpur IN'
-global.gurl = 'https://instagram.com/' // add your username
-global.devs = '94762280384';
-global.website = 'https://github.com/prabathLK/PRABATH-MD' //wa.me/+94762280384
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/H062oEI.jpeg'
+global.gurl = 'https://instagram.com/4.4.4.4.4.4.4444?igshid=MzNlNGNkZWQ4Mg==' // add your username
+global.devs = '919497705819';
+global.website = 'https://github.com/A-S-W-I-N-S-P-A-R-K-Y/X-BOT-MD' //wa.me/+919497705819
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/XjiMUp1.jpeg'
 module.exports = {
   OPENAI_API_KEY:process.env.OPENAI_API_KEY || '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',
-  botname: process.env.BOT_NAME || 'PRABATH-MD',
+  botname: process.env.BOT_NAME || 'DARKLEE MD',
   dl_limit: process.env.DL_SIZE || '300',
   art: process.env.AUTO_RESTART || 'true',
-  sudo: process.env.SUDO || '94762280384',
-  alivepic: process.env.ALIVE_IMG || 'https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg',
-  alivemsj: process.env.ALIVE_MSJ || '*POWERD BY PRABATH-MD* 🎊',
-  ownername:process.env.OWNER_NAME || 'PRABATH',
+  sudo: process.env.SUDO || '919497705819',
+  alivepic: process.env.ALIVE_IMG || 'https://i.imgur.com/EJgsxz0.jpeg',
+  alivemsj: process.env.ALIVE_MSJ || '*I AM ALIVE NOW POWERD BY DARKLEE-MD*⚠️',
+  ownername:process.env.OWNER_NAME || 'ANANDHU SER',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
-  author: process.env.PACK_INFO.split(";")[0] || 'author', 
-  auto_read_status : process.env.AUTO_READ_STATUS || 'false',
-  packname: process.env.PACK_INFO.split(";")[1] || 'Name',
+  author: process.env.PACK_INFO.split(";")[0] || 'ANANDHU SER', 
+  auto_read_status : process.env.AUTO_READ_STATUS || 'true',
+  packname: process.env.PACK_INFO.split(";")[1] || 'DARKLEE',
   autoreaction: process.env.AUTO_REACTION || 'off',
   antibadword : process.env.ANTI_BAD_WORD || 'fuck,huth,fucking,huththa,huththo,hukanna,fucked,pakaya,pako,ponnaya,ponnayo,kariya,kariyo,huththaa,hutta',
-  alwaysonline: process.env.ALWAYS_ONLINE || 'true',
+  alwaysonline: process.env.ALWAYS_ONLINE || 'false',
   antifake : process.env.FAKE_COUNTRY_CODE || '',
-  readmessage: process.env.READ_MESSAGE || true,
+  readmessage: process.env.READ_MESSAGE || false,
   HANDLERS: process.env.PREFIX || ['.'],
   warncount : process.env.WARN_COUNT || 3,
   disablepm: process.env.DISABLE_PM || "flase",
@@ -43,7 +43,7 @@ module.exports = {
   ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || '',
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || 'put-key-here',
   VERSION: process.env.VERSION === undefined ? 'v.5.0.0' : process.env.VERSION,
-  LANG: process.env.THEME|| 'PRABATH-MD',
+  LANG: process.env.THEME|| 'DARKLEE-MD',
   WORKTYPE: process.env.WORK_TYPE === undefined ? 'public' : process.env.WORK_TYPE
 };
 
